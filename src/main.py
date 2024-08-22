48849754f10d320e62ba6d852ecca33fde4c1519
IsabelMartinez2022 <101266828+IsabelMartinez2022@users.noreply.github.com>
2024-08-22 08:13:44 +0200

    retriving graph correctly

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from neo4j import GraphDatabase
+from model import *
 import pandas as pd
 import torch
-from torch_geometric.data import Data
+import numpy as np
+from torch_geometric.data import HeteroData
+from torch_geometric.transforms import RandomNodeSplit
 
-# To convert categorical values to numerical ones
+# For catgorical to numerical conversion
 sex_map = {'M': 0, 'F': 1}
 diagnosis_map = {'CN': 0, 'MCI': 1}
 
@@ -24,72 +27,97 @@ class GraphManager:
     def close(self):
         self.driver.close()
 
+    # Retrieving graph as a list of dictionaries
     def get_graph_data(self):
         with self.driver.session() as session:
             try:
-                # Retrieving nodes 
-                nodes = session.run("MATCH (n) RETURN elementId(n) AS id, labels(n) AS labels, properties(n) AS properties")
-                # session.run returns Record instances of object type Result
+                # Retrieving nodes
+                subject_nodes = session.run("MATCH (s:Subject) RETURN s.subject_id AS id, s.diagnosis AS diagnosis, s.age AS age, s.sex AS sex")
+                region_nodes = session.run("MATCH (r:Region) RETURN r.roi_id AS id, r.name AS name")
                 # Retrieving edges
-                edges = session.run("MATCH (n)-[r]->(m) RETURN elementId(n) AS source, elementId(m) AS target, type(r) AS type, properties(r) AS properties")
+                has_region_edges = session.run("MATCH (s:Subject)-[h:HAS_REGION]-(r:Region) RETURN s.subject_id AS source, r.roi_id AS target,"
+                                               "h.gm_volume AS gm_volume, h.regional_ct AS regional_ct")
+                functionally_connected_edges= session.run("MATCH (r1:Region)-[f:IS_FUNCTIONALLY_CONNECTED]-(r2:Region) RETURN r1.roi_id "
+                                                             "AS source, r2.roi_id AS target, f.corr_mci AS corr_mci, f.corr_cn AS corr_cn")
                 
-                # Converting to DataFrame format for the creation of tensors
-                nodes_df = pd.DataFrame([record.data() for record in nodes])
-                # record.data() to obtain record as dictionary
-                edges_df = pd.DataFrame([record.data() for record in edges])
-                return nodes_df, edges_df
+                # record.data() for dictionary conversion
+                subject_nodes = [record.data() for record in subject_nodes]
+                region_nodes = [record.data() for record in region_nodes]
+                has_region_edges = [record.data() for record in has_region_edges]
+                functionally_connected_edges = [record.data() for record in functionally_connected_edges]
+                return subject_nodes, region_nodes, has_region_edges, functionally_connected_edges
             except Exception as e:
                 print(f"Error retrieving graph data: {e}")
-                return pd.DataFrame(), pd.DataFrame()
+                return [], [], [], []
+
 
 # Conversion to suitable data types
-def transform_data(nodes_df, edges_df):
-    nodes_df = nodes_df.fillna('')  # In case of NaN values, it replaces them with empty strings
+def transform_data(subject_nodes, region_nodes, has_region_edges, functionally_connected_edges):
 
-    # Maps to numerical type using a dictionary of properties for each node
-    def map_edge_properties(edge_type, properties):
-    # Ensure properties is a dictionary
-        if not isinstance(properties, dict):
-            properties = {}
+    # Converting to pandas DataFrame format for the creation of tensors
+    subject_df = pd.DataFrame(subject_nodes)
+    region_df = pd.DataFrame( region_nodes)
+    has_region_df = pd.DataFrame(has_region_edges)
+    functionally_connected_df = pd.DataFrame(functionally_connected_edges)
 
-        if edge_type == 'has_region':
-            # Extract features for 'has_region' type edges
-            return [
-            float(properties.get('gm_volume', 0)),  # Default to 0 if key is missing
-            float(properties.get('regional_ct', 0))  # Default to 0 if key is missing
-        ]
-        else:  # Handle other edge types
-            return [
-            float(properties.get('median_correlation', 0))  # Default to 0 if key is missing
-        ]
-
-    # Apply the mapping function to the edges DataFrame
-    edges_df['edge_features'] = edges_df.apply(lambda row: map_edge_properties(row['type'], row['properties']), axis=1)
+    # Maps categorical values to numerical ones
+    subject_df['sex'] = subject_df['sex'].map(sex_map)
+    subject_df['diagnosis'] = subject_df['diagnosis'].map(diagnosis_map)
+    # Subject_id is a string so it is converted to an integer taking the last four digits
+    has_region_df['source'] = (has_region_df['source'].str[-4:]).astype(int)
 
     # Conversion to PyTorch tensors
-    edge_index = torch.tensor(edges_df[['source', 'target']].values.T, dtype=torch.long).contiguous()
-    edge_features = torch.tensor(edges_df['edge_features'].tolist(), dtype=torch.float)
+    # PyG object describing a heterogeneous graph (multiple node and/or edge types) in disjunct storage objects
+    data = HeteroData()
     
-    # We specify the node property "features" as the zero-layer node embeddings
-    features = torch.tensor(nodes_df['features'].tolist(), dtype=torch.float)
-    # We specify the node property "diagnosis" as class labels
-    labels = torch.tensor(labels.tolist(), dtype=torch.long)
+    # Subject node feature matrix
+    data['subject'].x = torch.tensor(subject_df[['age', 'sex']].values, dtype=torch.float)
+    # Target to train against 
+    data['subject'].y = torch.tensor(subject_df['diagnosis'].values, dtype=torch.long)
 
-    # PyG Data object
-    data = Data(x=features, edge_index=edge_index, edge_attr=edge_features,y=labels)
-    
+    # Region node feature matrix
+    # Using index as dummy feature
+    data['region'].x = torch.tensor(region_df.index.values, dtype=torch.float)  
+
+    # Graph connectivity with shape [2, num_edges] for has_region edges
+    # Convert list to a single array to avoid efficiency warning
+    has_region_edge_index = np.array([has_region_df['source'].values, has_region_df['target'].values])
+    data[('subject', 'has_region', 'region')].edge_index = torch.tensor(
+        has_region_edge_index, dtype=torch.long)
+    # Has_region edge feature matrix with shape [num_edges, num_edge_features]= [num_edges, 2]
+    data[('subject', 'has_region', 'region')].edge_attr = torch.tensor(
+        has_region_df[['gm_volume', 'regional_ct']].values, dtype=torch.float)
+
+    # Graph connectivity with shape [2, num_edges] for is_functionally_connected edges
+    functionally_connected_edge_index= np.array([functionally_connected_df['source'].values, 
+        functionally_connected_df['target'].values])
+    data[('region', 'is_functionally_connected', 'region')].edge_index = torch.tensor(
+        functionally_connected_edge_index, dtype=torch.long)
+    # Is_functionally_connected edge feature matrix with shape [num_edges, num_edge_features]= [num_edges, 2]
+    data[('region', 'is_functionally_connected', 'region')].edge_attr = torch.tensor(
+        functionally_connected_df[['corr_mci', 'corr_cn']].values, dtype=torch.float
+    )
+    print(data.is_undirected()) # Returns false
     return data
 
 def main():
     # Initialize GraphManager and retrieve data
     graph_manager = GraphManager(uri, auth)
-    nodes_df, edges_df = graph_manager.get_graph_data()
+    subject_nodes, region_nodes, has_region_edges, functionally_connected_edges = graph_manager.get_graph_data()
     
     # Preprocess data
-    data = transform_data(nodes_df, edges_df)
+    data = transform_data(subject_nodes, region_nodes, has_region_edges, functionally_connected_edges)
     
-    print("Data Transformation Completed")
+    print("Data transformation completed.")
     print(data)
+
+    # Optionally split the data into train/test sets
+    transform = RandomNodeSplit(num_test=0.1, num_val=0.1)
+    data = transform(data)
+    prediction= GCN()
+    optimizer = torch.optim.Adam(prediction.parameters(), lr=0.01, weight_decay=5e-4)
+    prediction.training(data,optimizer)
+    prediction.evaluation(data,optimizer)
 
     # Close connection
     graph_manager.close()
--- src/model.py
+++ src/model.py
@@ -1,29 +1,6 @@
 import torch
-from torch_geometric.data import Data
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
-from torch_geometric.transforms import RandomNodeSplit
-
-# Create a mapping from node IDs to consecutive indices
-node_id_map = {node_id: idx for idx, node_id in enumerate(nodes_df['id'])}
-
-# Prepare edge_index
-edges_df['source'] = edges_df['source'].map(node_id_map)
-edges_df['target'] = edges_df['target'].map(node_id_map)
-edge_index = torch.tensor(edges_df[['source', 'target']].values.T, dtype=torch.long).contiguous()
-
-# Prepare node features and labels
-features = torch.tensor(nodes_df[['features']].values.tolist(), dtype=torch.float)  # Adjust depending on feature format
-labels = torch.tensor(nodes_df['label'].values, dtype=torch.long)
-
-# Create PyG Data object
-data = Data(x=features, edge_index=edge_index, y=labels)
-
-# Optionally split the data into train/test sets
-transform = RandomNodeSplit(num_test=0.1, num_val=0.1)
-data = transform(data)
-
-print(data)
 
 # Define the GCN model
 class GCN(torch.nn.Module):
@@ -39,3 +16,20 @@ class GCN(torch.nn.Module):
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
         return F.log_softmax(x, dim=1)
+    
+    def training(self,data,optimizer):
+        self.train()
+        for epoch in range(200):
+            optimizer.zero_grad()
+            out = self(data)
+            loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+            loss.backward()
+            optimizer.step()
+    
+    def evaluation(self, data):
+        self.eval()
+        pred = self(data).argmax(dim=1)
+        correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
+        acc = int(correct) / int(data.test_mask.sum())
+        print(f'Accuracy: {acc:.4f}')
+    
